6471822f7bdf773a7361b11abe361f2da6c890dc
Johan Karlsson <johan.k0528@gmail.com>
2022-11-10 15:09:01 +0100

    Add thread to handle commands

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,36 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::thread::spawn(|| {
+        loop {
+            println!("Waiting for commands...");
+            // Read input for commands
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            // If the input is "exit", exit the program
+            if input.trim() == "exit" {
+                println!("Type 'confirm' to exit the program");
+                let mut confirm: String = String::new();
+                std::io::stdin().read_line(&mut confirm).unwrap();
+                if confirm.trim() == "confirm" {
+                    std::process::exit(0);
+                } else {
+                    continue;
+                }
+            }
+        }
+    });
+
     println!("Started Server on http://127.0.0.1:8080");
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .service(ping)
-            .route("/hey", web::get().to(manual_hello))
+            .service(web::resource("/manual_hello").to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8080))
+    .expect("Can not bind to port 8080")
     .run()
     .await
 }
